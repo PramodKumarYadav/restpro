d9fbb8933c5c94c3a3a618317f80451b47e1a14d
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2022-04-15 23:54:02 +0200

    Feature add junit5 (#8)
    * test to directly push code to main.
    
    * added test sandox class
    
    * added pom file.
    
    * updated readme file.
    
    * ignore target repository
    
    Co-authored-by: pramod.yadav <pramodyadav-cbh>

---
+++ src/test/java/TestSandbox.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestSandbox {
+    /**
+     * a very basic test
+     */
+    @Test
+    void assertThatTrueIsTrue(){
+        assertTrue(true, "true is true");
+    }
+
+    @Test
+    void assertThatADayIsADay(){
+        assertEquals("day", "day", "true is true");
+    }
+}

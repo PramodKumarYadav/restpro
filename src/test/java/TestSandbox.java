2701fbd8be39afa28c77646300f14e541c7827f4
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2022-04-25 23:32:08 +0200

    Add failing and flaky testcases (#18)
    * test to directly push code to main.
    
    * testing git user.name
    
    * removed test line.
    
    * added flaky testcases
    
    Co-authored-by: Pramod Yadav <pramodyadav@gmail.com>

--- src/test/java/TestSandbox.java
+++ src/test/java/TestSandbox.java
@@ -1,4 +1,5 @@
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,8 +14,22 @@ public class TestSandbox {
         assertTrue(true, "true is true");
     }
 
+    @Tag("failing")
     @Test
     void assertThatADayIsADay() {
-        assertEquals("day", "day", "true is true");
+        assertEquals("day", "night", "true is true");
+    }
+
+    @Tag("flaky")
+    @Test
+    void createAFlakyTestCase() {
+        long currentTimeStamp = System.currentTimeMillis();
+        // Todo: Remove this line with a logging statement
+        System.out.println(currentTimeStamp);
+        if (currentTimeStamp % 2 == 0) {
+            assertTrue(true, "time is even");
+        } else {
+            assertTrue(false, "time is odd");
+        }
     }
 }

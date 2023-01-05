1446a3e7fb88ccf3ee8db20e55e3e975f5fecff3
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2023-01-05 15:55:33 +0100

    Add custom annotations (#65)
    * test to directly push code to main.
    
    * testing git user.name
    
    * removed test line.
    
    * adding lombok and log4j/slf4j dependencies
    
    * adding settings to update java configuration automatically
    
    * add log4j properties file
    
    * updated readme file with badges
    
    * update tests with logger examples
    
    * renamed license file
    
    * add custom annotations
    
    Co-authored-by: Pramod Yadav <pramodyadav@gmail.com>

--- src/test/java/TestSandbox.java
+++ src/test/java/TestSandbox.java
@@ -2,10 +2,12 @@
 import com.typesafe.config.Config;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.powertester.annotations.FailingTest;
+import org.powertester.annotations.FlakyTest;
+import org.powertester.annotations.SmokeTest;
 import org.powertester.config.TestEnvFactory;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -14,34 +16,32 @@ public class TestSandbox {
     @RepeatedTest(10)
     void assertThatWeCanGetUserConfig() {
         final Config CONFIG = TestEnvFactory.getInstance().getConfig();
-        log.info(TestEnvFactory.getInstance().getConfig().getString("TEST_ENV"));
-        log.info(CONFIG.getString("CREATE_EMPLOYEE_ENDPOINT"));
-        log.info(CONFIG.getString("ADMIN_NAME"));
 
-        Boolean flag = CONFIG.getBoolean("TOGGLE");
-        log.info(String.valueOf(flag));
-
-        log.info(String.valueOf(CONFIG.getInt("NR_OF_USERS")));
-        log.info(String.valueOf(CONFIG.getDouble("PRICE")));
-        log.info(CONFIG.getString("USER_NAME"));
+        assertAll("Config test",
+                () -> assertEquals("DEVELOP", CONFIG.getString("TEST_ENV"), "TEST_ENV"),
+                () -> assertEquals("/employee/create", CONFIG.getString("CREATE_EMPLOYEE_ENDPOINT"), "CREATE_EMPLOYEE_ENDPOINT"),
+                () -> assertEquals("develop-admin-user", CONFIG.getString("ADMIN_NAME"), "ADMIN_NAME"),
+                () -> assertEquals(false, CONFIG.getBoolean("TOGGLE"), "TOGGLE"),
+                () -> assertEquals(10, CONFIG.getInt("NR_OF_USERS"), "NR_OF_USERS"),
+                () -> assertEquals(123.456, CONFIG.getDouble("PRICE"), "PRICE"),
+                () -> assertEquals("develop-user", CONFIG.getString("USER_NAME"), "USER_NAME")
+        );
     }
 
     /**
      * a very basic test
      */
-    @Test
+    @SmokeTest
     void assertThatTrueIsTrue() {
         assertTrue(true, "true is true");
     }
 
-    @Tag("failing")
-    @Test
+    @FailingTest
     void assertThatADayIsADay() {
         assertEquals("day", "night", "true is true");
     }
 
-    @Tag("flaky")
-    @Test
+    @FlakyTest
     void createAFlakyTestCase() {
         long currentTimeStamp = System.currentTimeMillis();
         log.debug("currentTimeStamp: {}", currentTimeStamp);
@@ -50,13 +50,5 @@ public class TestSandbox {
         } else {
             assertTrue(false, "time is odd");
         }
-    }
-
-    @Test
-    void testLogLevels(){
-        log.info("this is info statement");
-        log.debug("this is debug statement");
-        log.warn("this is warning statement");
-        log.error("this is error statement");
     }
 }

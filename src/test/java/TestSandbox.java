3f0d0d9a8d184ad9be9999edee49c08ec1f74a37
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2023-02-26 21:20:36 +0100

    Add pre commit formatting hook (#101)
    * test to directly push code to main.
    
    * testing git user.name
    
    * removed test line.
    
    * adding lombok and log4j/slf4j dependencies
    
    * adding settings to update java configuration automatically
    
    * add log4j properties file
    
    * updated readme file with badges
    
    * update tests with logger examples
    
    * renamed license file
    
    * added pre commit config file
    
    * poorly formated file
    
    ---------
    
    Co-authored-by: Pramod Yadav <pramodyadav@gmail.com>

--- src/test/java/TestSandbox.java
+++ src/test/java/TestSandbox.java
@@ -1,7 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.typesafe.config.Config;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.RepeatedTest;
 import org.powertester.annotations.FailingTest;
 import org.powertester.annotations.FlakyTest;
@@ -12,49 +15,52 @@ import setup.TestSetup;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @Slf4j
 public class TestSandbox extends TestSetup {
-    @RepeatedTest(10)
-    void assertThatWeCanGetUserConfig() {
-        final Config CONFIG = TestEnvFactory.getInstance().getConfig();
-        final String expectedEnv = CONFIG.getString("TEST_ENV");
-        assertAll("Config test",
-                () -> assertEquals(expectedEnv, CONFIG.getString("TEST_ENV"), "TEST_ENV"),
-                () -> assertEquals("/employee/create", CONFIG.getString("CREATE_EMPLOYEE_ENDPOINT"), "CREATE_EMPLOYEE_ENDPOINT"),
-                () -> assertEquals(expectedEnv.toLowerCase()+ "-admin-user", CONFIG.getString("ADMIN_NAME"), "ADMIN_NAME"),
-                () -> assertFalse(CONFIG.getBoolean("TOGGLE"), "TOGGLE"),
-                () -> assertEquals(10, CONFIG.getInt("NR_OF_USERS"), "NR_OF_USERS"),
-                () -> assertEquals(123.456, CONFIG.getDouble("PRICE"), "PRICE"),
-                () -> assertEquals(expectedEnv.toLowerCase() + "-user", CONFIG.getString("USER_NAME"), "USER_NAME")
-        );
-    }
+  @RepeatedTest(10)
+  void assertThatWeCanGetUserConfig() {
+    final Config CONFIG = TestEnvFactory.getInstance().getConfig();
+    final String expectedEnv = CONFIG.getString("TEST_ENV");
+    assertAll(
+        "Config test",
+        () -> assertEquals(expectedEnv, CONFIG.getString("TEST_ENV"), "TEST_ENV"),
+        () ->
+            assertEquals(
+                "/employee/create",
+                CONFIG.getString("CREATE_EMPLOYEE_ENDPOINT"),
+                "CREATE_EMPLOYEE_ENDPOINT"),
+        () ->
+            assertEquals(
+                expectedEnv.toLowerCase() + "-admin-user",
+                CONFIG.getString("ADMIN_NAME"),
+                "ADMIN_NAME"),
+        () -> assertFalse(CONFIG.getBoolean("TOGGLE"), "TOGGLE"),
+        () -> assertEquals(10, CONFIG.getInt("NR_OF_USERS"), "NR_OF_USERS"),
+        () -> assertEquals(123.456, CONFIG.getDouble("PRICE"), "PRICE"),
+        () ->
+            assertEquals(
+                expectedEnv.toLowerCase() + "-user", CONFIG.getString("USER_NAME"), "USER_NAME"));
+  }
 
-    /**
-     * a very basic test
-     */
-    @SmokeTest
-    void assertThatTrueIsTrue() {
-        assertTrue(true, "true is true");
-    }
+  /** a very basic test */
+  @SmokeTest
+  void assertThatTrueIsTrue() {
+    assertTrue(true, "true is true");
+  }
 
-    @FailingTest
-    void assertThatADayIsADay() {
-        assertEquals("day", "night", "true is true");
-    }
+  @FailingTest
+  void assertThatADayIsADay() {
+    assertEquals("day", "night", "true is true");
+  }
 
-    @FlakyTest
-    void createAFlakyTestCase() {
-        long currentTimeStamp = System.currentTimeMillis();
-        log.debug("currentTimeStamp: {}", currentTimeStamp);
-        if (currentTimeStamp % 2 == 0) {
-            assertTrue(true, "time is even");
-        } else {
-            assertTrue(false, "time is odd");
-        }
+  @FlakyTest
+  void createAFlakyTestCase() {
+    long currentTimeStamp = System.currentTimeMillis();
+    log.debug("currentTimeStamp: {}", currentTimeStamp);
+    if (currentTimeStamp % 2 == 0) {
+      assertTrue(true, "time is even");
+    } else {
+      assertTrue(false, "time is odd");
     }
+  }
 }

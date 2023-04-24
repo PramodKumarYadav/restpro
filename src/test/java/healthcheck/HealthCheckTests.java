2f38b1c7f41c9ee7686fa966119519fcfa6ef1d3
pramodkumaryadav <pramodyadav027@gmail.com>
2023-04-24 18:15:37 +0200

    add health checks

---
+++ src/test/java/healthcheck/HealthCheckTests.java
@@ -0,0 +1,19 @@
+package healthcheck;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.powertester.annotations.HealthCheckTest;
+import org.powertester.healthcheck.HealthCheckAPI;
+import setup.TestSetup;
+
+@Slf4j
+public class HealthCheckTests extends TestSetup {
+  @HealthCheckTest
+  void assertThatRestfulBookerApplicationIsUpAndHealthy() {
+    // Act
+    Response response = HealthCheckAPI.healthCheck();
+    assertEquals(201, response.getStatusCode());
+  }
+}

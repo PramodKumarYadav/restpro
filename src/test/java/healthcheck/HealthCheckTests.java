be502516c105de41762598f5b9f5efb4a7b661ad
pramodkumaryadav <pramodyadav027@gmail.com>
2023-04-24 18:23:41 +0200

    changed back to 201

--- src/test/java/healthcheck/HealthCheckTests.java
+++ src/test/java/healthcheck/HealthCheckTests.java
@@ -14,6 +14,6 @@ public class HealthCheckTests extends TestSetup {
   void assertThatRestfulBookerApplicationIsUpAndHealthy() {
     // Act
     Response response = HealthCheckAPI.healthCheck();
-    assertEquals(200, response.getStatusCode());
+    assertEquals(201, response.getStatusCode());
   }
 }

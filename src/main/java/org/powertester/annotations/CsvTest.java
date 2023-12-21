2e896008a52a6d54a12a3f917ee73b3031a1ffd4
pramodkumaryadav <pramodyadav027@gmail.com>
2023-12-21 22:58:50 +0100

    format annotation

--- src/main/java/org/powertester/annotations/CsvTest.java
+++ src/main/java/org/powertester/annotations/CsvTest.java
@@ -1,11 +1,10 @@
 package org.powertester.annotations;
 
-import org.junit.jupiter.params.ParameterizedTest;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.junit.jupiter.params.ParameterizedTest;
 
 @Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)

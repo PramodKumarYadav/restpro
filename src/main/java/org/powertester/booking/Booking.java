27874cbf41c0841f75f3ddecef3d7e3107f7f599
pramodkumaryadav <pramodyadav027@gmail.com>
2023-12-22 12:20:25 +0100

    make values dynamic

--- src/main/java/org/powertester/booking/Booking.java
+++ src/main/java/org/powertester/booking/Booking.java
@@ -1,12 +1,14 @@
 package org.powertester.booking;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.concurrent.TimeUnit;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
+import net.datafaker.Faker;
 import org.powertester.booking.entitites.Bookingdates;
 
 @Slf4j
@@ -26,16 +28,19 @@ public class Booking {
 
   public static Booking getInstance() {
     Bookingdates bookingdates =
-        Bookingdates.builder().setCheckin("2018-01-01").setCheckout("2019-01-01").build();
+        Bookingdates.builder()
+            .setCheckin(new Faker().date().future(1, TimeUnit.DAYS, "YYYY-MM-dd"))
+            .setCheckout(new Faker().date().future(6, TimeUnit.DAYS, "YYYY-MM-dd"))
+            .build();
 
     Booking booking =
         Booking.builder()
-            .setFirstname("Jim")
-            .setLastname("Brown")
-            .setTotalprice(111)
+            .setFirstname(new Faker().name().firstName())
+            .setLastname(new Faker().name().lastName())
+            .setTotalprice(new Faker().number().numberBetween(1, 1000))
             .setDepositpaid(true)
             .setBookingdates(bookingdates)
-            .setAdditionalneeds("Breakfast")
+            .setAdditionalneeds(new Faker().food().dish())
             .build();
 
     log.info("bookingBody: {}", booking);

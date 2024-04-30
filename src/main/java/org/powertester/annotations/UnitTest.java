941fff03a98c71b2a38929d471b5913b561bec80
pramodkumaryadav <pramodyadav027@gmail.com>
2024-04-30 23:16:43 +0200

    updated workflows

---
+++ src/main/java/org/powertester/annotations/UnitTest.java
@@ -0,0 +1,12 @@
+package org.powertester.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.Tag;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("unit")
+public @interface UnitTest {}
